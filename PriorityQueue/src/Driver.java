7d480de4794669a045eb40a7c4f156a7cf4cb957
Austin Klum <Austin Klum@DESKTOP-DCSDHIN>
2017-09-26 17:08:35 -0500

    Minor Changes that need to be committed

--- PriorityQueue/src/Driver.java
+++ PriorityQueue/src/Driver.java
@@ -3,7 +3,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		PriorityQueue myQ = new PriorityQueue(7);
-		for(int i = 1; i <= 7; i++) {
+		for(int i = 7; i >= 1; i--) {
 			myQ.insert(i, "Data" + i);
 		}
 		System.out.println(myQ);
