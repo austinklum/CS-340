cc33b4b4fc184a8acd9787eaae6d6c6aefd24430
Austin Klum <Austin Klum@DESKTOP-DCSDHIN>
2017-09-29 17:32:00 -0500

    Added basic file processing

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -8,7 +8,6 @@ Duplicates are recorded by a count field associated with the int
 */ 
     final int CREATE = 0;
     final int REUSE = 1; 
-    final String fileName = "ppt-Tree";
     private RandomAccessFile f; 
     long root; //the address of the root node in the file 
     long free; //the address in the file of the first node in the free list
@@ -18,58 +17,107 @@ private class Node {
     private int data; 
     private int count; 
     private long right;
+    private long addr;
     
-    private Node(long L,int d, long r) { 
+    private Node(long L,int d, long r, long a) { 
     //constructor for a new node 
         data  = d;
         count = 1;
         left  = L;
         right = r;
+        addr  = a;
            
     } 
     private Node(long addr) throws IOException{ 
     //constructor for a node that exists and is stored in the file
-        f = new RandomAccessFile(fileName,"r");
+       // f = new RandomAccessFile(fileName,"r");
         f.seek(addr);
         
         data  = f.readInt();
         count = f.readInt();
         left  = f.readLong();
         right = f.readLong();
+        this.addr = addr;
         
     } 
-    private void writeNode(long addr) throws IOException { 
+    private void writeNode() throws IOException { 
     //writes the node to the file at location addr
+        f.seek(addr);
+        f.writeInt(data);
+        f.writeInt(count);
+        f.writeLong(left);
+        f.writeLong(right);
+    }
+    private void writeNode(Long addr) throws IOException {
+       new Node(addr).writeNode();
     }
 }
 
-    public BinarySearchTree(String fname,int mode) throws IOException{ 
+    public BinarySearchTree(String fname, int mode) throws IOException{ 
     //if mode is CREATE a new empty file is created 
     //if mode is CREATE and a file with file name fname exists the file with fname must be
     //deleted before the new empty file is created 
     //if mode is REUSE an existing file is used if it exists otherwise a new empty file
     //is created 
-    
+        File path = new File("tree.txt");
+        if (path.exists() && mode == CREATE) {
+            path.delete();
+        } 
+        
+        f = new RandomAccessFile(path,"rw");
+        
+        if(mode == CREATE) {
+            root = 0;
+            free = 0;
+            f.writeLong(root);
+            f.writeLong(free);
+        } else {
+            f.seek(0);
+            root = f.readLong();
+            free = f.readLong();
+        }
+        
     }
-    public void insert(int d) throws IOException{ 
+    public void insert(int d) throws IOException { 
     //insert d into the tree
     //if d is in the tree increment the count field associated with d
         root = insert(new Node(root), d);
+        new Node(root).writeNode();
     } 
     
-    private Long insert(Node node, int d) {
-        if (node == null) return free; //Case when we walked off tree
-        if (node.data > d)  node.left = insert(new Node(node.left), d); //Case when we need to look left
-        else if (node.data < d) node.right = insert(new Node(node.right), d); //Case when we need to look right
-        else    node.count++; //Case when a duplicate
-        return node;
+    private Long insert(Node node, int d) throws IOException {
+        if (node == null) {
+            return free; //Case when we walked off tree. getFree();?
+        }
+        if (node.data > d) {
+            node.left = insert(new Node(node.left), d); //Case when we need to look left
+            node.writeNode();
+        }
+        else if (node.data < d) {
+            node.right = insert(new Node(node.right), d); //Case when we need to look right
+            node.writeNode();
+        }
+        else {
+            node.count++; //Case when a duplicate
+            node.writeNode();
+        }
+        
+        return node.addr; //Base case. Return self.
     }
     
     public int find(int d) throws IOException{ 
     //if d is in the tree return the value of count associated with d 
-    //otherwise return0 
-        return 0;
+    //otherwise return0
+        return find(new Node(root), d);
     }
+    
+    private int find(Node r, int d) throws IOException {
+        if(r == null) return 0;
+        if (r.data > d) return find(new Node(r.left), d);
+        if (r.data < d) return find(new Node(r.right), d);
+        return r.count;
+    }
+    
     public void removeOne(int d) throws IOException{ 
     //remove one copy of d from the tree
     //if the copy is the last copy removed from the tree
