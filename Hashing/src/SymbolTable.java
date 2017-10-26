fdf488e4d8742bc69dfd64cc49c524b20de2a34c
Austin Klum <klumaustin@gmail.com>
2017-10-26 18:29:05 -0500

    Remove works when just a head

--- Hashing/src/SymbolTable.java
+++ Hashing/src/SymbolTable.java
@@ -1,4 +1,6 @@
 import java.util.*;
+
+import javax.security.auth.kerberos.KerberosKey;
 public class SymbolTable implements Iterable<String> {
     private class Node {
     //a node used to build linked lists
@@ -44,9 +46,15 @@ public class SymbolTable implements Iterable<String> {
     }
     public boolean find(String k) {
     //return true if k is in the table otherwise return false 
+        System.out.println("Finding...");
         if(table[hash(k)] == null) {
+            System.out.println("Head is null: Retrun false");
             return false;
         }
+        System.out.println("Head is something find if it's not null");
+        Node thing = findAux(k);
+        System.out.println("auxVal in find : " + thing.key);
+        System.out.println("Did findAux find something in find()? : " + thing != null);
         return findAux(k) != null;
     }
     
@@ -70,7 +78,7 @@ public class SymbolTable implements Iterable<String> {
             }
             temp = temp.next;
         }
-        
+        System.out.println("Returning from aux : " + temp.key);
         return temp;
     }
     public Object getData(String k) { 
@@ -98,6 +106,45 @@ public class SymbolTable implements Iterable<String> {
             temp.data = d;
         }
     }
+    
+    public boolean remove(String k) { 
+        //if k is in the table, return the entry for k and return true 
+        //if k is not in the table, return false
+        boolean inTable = find(k);
+        System.out.println("inTable = " + inTable);
+        if(inTable) {
+            //Set temp to head of linked list
+            Node temp = table[hash(k)];
+            System.out.println("temp = " + temp.key);
+            if(temp.key.equals(k)) {
+                System.out.println("That happened");
+                //This can only happen if we were at the head and the next thing didnt exist.
+                if(temp.next == null) {
+                    table[hash(k)] = null;
+                }else {
+                    temp.next = temp.next.next;
+                }
+                //We might want to return here?
+                return inTable;
+            }
+            System.out.println("This is a test");
+            //Loop through list until null or the key
+            System.out.println("Temp.next != Null is " + temp.next != null);
+            System.out.println("Temp.next.key is " + temp.next.key);
+            while (temp.next != null || temp.next.key.equals(k)) {
+                temp = temp.next;
+                System.out.println("In while?");
+            }
+            System.out.println("Skipping it!");
+            //We are one before the key value
+            //Skip over the key value to the next value.
+            temp.next = temp.next.next;
+        }
+        return inTable;
+    } 
+    
+
+    
     public class STIterator implements Iterator<String> { 
         //An iterator that iterates through the keys in the table
         private Node indexNode;
@@ -109,10 +156,10 @@ public class SymbolTable implements Iterable<String> {
             nextIndex = 0;
             indexNode = table[nextIndex];
             nextIndex++;
-            System.out.println("I was made!");
+           // System.out.println("I was made!");
         } 
         public boolean hasNext() {
-            System.out.println("HasNext() = " + nextIndex);
+            //System.out.println("HasNext() = " + nextIndex);
             return nextIndex < table.length;
 
         }
@@ -127,19 +174,18 @@ public class SymbolTable implements Iterable<String> {
             /*If I am at the end of a list of a specific row in the table.
               Go to the next row in the table */
             //While I dont have a next node look at the next table index
-            while (!hasNextNode() /* || indexNode.data != null*/) {
+            while ((!hasNextNode() && nextIndex <= table.length)/* || indexNode.data != null*/) {
                 indexNode = table[nextIndex];
                 nextIndex++;
                 if(indexNode != null) {
                     break;
                 }
-                System.out.printf("indexNode = %b : nextIndex = %d\n", indexNode == null, nextIndex);
-                if(nextIndex <= table.length) {
-                    return "We are at the end folks!";
-                }
+               // System.out.printf("indexNode = %b : nextIndex = %d\n", indexNode == null, nextIndex);
+//                if(nextIndex <= table.length) {
+//                    nextIndex = table.length + 1;
+//                }
             }
             
-            //I must have a value by now or indexOutOfBounds
             Node retVal = indexNode;
             indexNode = indexNode.next;
 
@@ -150,28 +196,6 @@ public class SymbolTable implements Iterable<String> {
         } 
     }
     
-    public boolean remove(String k) { 
-    //if k is in the table, return the entry for k and return true 
-    //if k is not in the table, return false
-        boolean inTable = find(k);
-        if(inTable) {
-            //Set temp to head of linked list
-            Node temp = table[hash(k)];
-            if(temp.data.equals(k)) {
-                temp.next = temp.next.next;
-                //We might want to return here?
-            }
-           //Loop through list until null or the key
-            while (temp.next != null || temp.next.key.equals(k)) {
-                temp = temp.next;
-            }
-            //We are one before the key value
-            //Skip over the key value to the next value.
-            temp.next = temp.next.next;
-        }
-        return inTable;
-    } 
-    
     public Iterator<String> iterator() { 
     //return a new STIterator object 
         return (Iterator<String>)new STIterator();
@@ -181,9 +205,16 @@ public class SymbolTable implements Iterable<String> {
         SymbolTable myTable = new SymbolTable(5);
         myTable.insert("1");
         myTable.setValue("1", "The first thing");
-        myTable.insert("2");
+        myTable.insert("achoo");
         myTable.setValue("achoo", "The second thing");
+        myTable.insert("I really need to pee");
+        myTable.setValue("I really need to pee", "Well too bad!");
         System.out.println("Printing out table");
+        for (String string : myTable) {
+            System.out.println(string);
+        }
+        myTable.remove("I really need to pee");
+        System.out.println("Removed well too bad!");
         for (String string : myTable) {
             System.out.println(string);
         }
